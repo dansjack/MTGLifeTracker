114dbc80dfda6825c56546ddebe390a363caed57
dsjack <dsjack89@gmail.com>
2019-07-31 13:07:31 -0700

    Refactors most of GameActivty code into a cascading switch statement

--- app/src/main/java/com/team2/mtglifetracker/GameActivity.java
+++ app/src/main/java/com/team2/mtglifetracker/GameActivity.java
@@ -38,13 +38,6 @@ public class GameActivity extends AppCompatActivity {
 
         // determine content view
         switch (playerCount) {
-            case 2:
-                if (layoutType == 2) {
-                    setContentView(R.layout.activity_two_player2);
-                } else if (layoutType == 1) {
-                    setContentView(R.layout.activity_two_player1);
-                }
-                break;
             case 4:
                 if (layoutType == 2) {
                     setContentView(R.layout.activity_four_player2);
@@ -52,147 +45,120 @@ public class GameActivity extends AppCompatActivity {
                     setContentView(R.layout.activity_four_player1);
                 }
                 break;
+            case 2:
+                if (layoutType == 2) {
+                    setContentView(R.layout.activity_two_player2);
+                } else if (layoutType == 1) {
+                    setContentView(R.layout.activity_two_player1);
+                }
+                break;
             default:
                 setContentView(R.layout.activity_three_player);
         }
 
-        // get reset button
-        ImageButton optBtn = findViewById(R.id.playerOptBtn);
+        // Stores buttons, player scores, and sets onClick listeners according to player count
+        switch (playerCount) {
+            case 4:
+                Button p4UpBtn = findViewById(R.id.p4UpBtn);
+                Button p4DownBtn = findViewById(R.id.p4DownBtn);
 
-        // get up/down buttons for each player
-        Button p1UpBtn = findViewById(R.id.p1UpBtn);
-        Button p1DownBtn = findViewById(R.id.p1DownBtn);
-
-        Button p2UpBtn = findViewById(R.id.p2UpBtn);
-        Button p2DownBtn = findViewById(R.id.p2DownBtn);
-
-        // get current scores for each player
-        final TextView p1CurrScore = findViewById(R.id.p1Score);
-        final TextView p2CurrScore = findViewById(R.id.p2Score);
-
-        // set up click listeners for the up/down buttons:
-        // player 1
-        p1UpBtn.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                // do something when up button clicked
-                int score = Integer.parseInt(p1CurrScore.getText().toString());
-                p1CurrScore.setText(String.valueOf(score + 1));
-            }
-        });
-
-        p1DownBtn.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                int score = Integer.parseInt(p1CurrScore.getText().toString());
-                p1CurrScore.setText(String.valueOf(score - 1));
-            }
-        });
-
-        // player 2
-        p2UpBtn.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                int score = Integer.parseInt(p2CurrScore.getText().toString());
-                p2CurrScore.setText(String.valueOf(score + 1));
-            }
-        });
-
-        p2DownBtn.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                int score = Integer.parseInt(p2CurrScore.getText().toString());
-                p2CurrScore.setText(String.valueOf(score - 1));
-            }
-        });
+                final TextView p4CurrScore = findViewById(R.id.p4Score);
 
 
-        // set up click listeners for the options:
-        //   reset
-        optBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(GameActivity.this, "Options Button clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
-        if (playerCount == 3) {
-            Button p3UpBtn = findViewById(R.id.p3UpBtn);
-            Button p3DownBtn = findViewById(R.id.p3DownBtn);
+                p4UpBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        int score = Integer.parseInt(p4CurrScore.getText().toString());
+                        p4CurrScore.setText(String.valueOf(score + 1));
+                    }
+                });
 
-            final TextView p3CurrScore = findViewById(R.id.p3Score);
+                p4DownBtn.setOnClickListener(new View.OnClickListener() {
 
-            // player 3
-            p3UpBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        int score = Integer.parseInt(p4CurrScore.getText().toString());
+                        p4CurrScore.setText(String.valueOf(score - 1));
+                    }
+                });
+            case 3:
+                Button p3UpBtn = findViewById(R.id.p3UpBtn);
+                Button p3DownBtn = findViewById(R.id.p3DownBtn);
 
-                @Override
-                public void onClick(View v) {
-                    int score = Integer.parseInt(p3CurrScore.getText().toString());
-                    p3CurrScore.setText(String.valueOf(score + 1));
-                }
-            });
+                final TextView p3CurrScore = findViewById(R.id.p3Score);
 
-            p3DownBtn.setOnClickListener(new View.OnClickListener() {
+                p3UpBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        int score = Integer.parseInt(p3CurrScore.getText().toString());
+                        p3CurrScore.setText(String.valueOf(score + 1));
+                    }
+                });
 
-                @Override
-                public void onClick(View v) {
-                    int score = Integer.parseInt(p3CurrScore.getText().toString());
-                    p3CurrScore.setText(String.valueOf(score - 1));
-                }
-            });
+                p3DownBtn.setOnClickListener(new View.OnClickListener() {
 
-        }
+                    @Override
+                    public void onClick(View v) {
+                        int score = Integer.parseInt(p3CurrScore.getText().toString());
+                        p3CurrScore.setText(String.valueOf(score - 1));
+                    }
+                });
+            case 2:
+                Button p1UpBtn = findViewById(R.id.p1UpBtn);
+                Button p1DownBtn = findViewById(R.id.p1DownBtn);
+                Button p2UpBtn = findViewById(R.id.p2UpBtn);
+                Button p2DownBtn = findViewById(R.id.p2DownBtn);
 
-        if (playerCount == 4) {
-            Button p3UpBtn = findViewById(R.id.p3UpBtn);
-            Button p3DownBtn = findViewById(R.id.p3DownBtn);
+                final TextView p1CurrScore = findViewById(R.id.p1Score);
+                final TextView p2CurrScore = findViewById(R.id.p2Score);
 
-            Button p4UpBtn = findViewById(R.id.p4UpBtn);
-            Button p4DownBtn = findViewById(R.id.p4DownBtn);
+                p1UpBtn.setOnClickListener(new View.OnClickListener() {
 
-            final TextView p3CurrScore = findViewById(R.id.p3Score);
-            final TextView p4CurrScore = findViewById(R.id.p4Score);
+                    @Override
+                    public void onClick(View v) {
+                        // do something when up button clicked
+                        int score = Integer.parseInt(p1CurrScore.getText().toString());
+                        p1CurrScore.setText(String.valueOf(score + 1));
+                    }
+                });
 
-            // player 3
-            p3UpBtn.setOnClickListener(new View.OnClickListener() {
+                p1DownBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        int score = Integer.parseInt(p1CurrScore.getText().toString());
+                        p1CurrScore.setText(String.valueOf(score - 1));
+                    }
+                });
 
-                @Override
-                public void onClick(View v) {
-                    int score = Integer.parseInt(p3CurrScore.getText().toString());
-                    p3CurrScore.setText(String.valueOf(score + 1));
-                }
-            });
+                // player 2
+                p2UpBtn.setOnClickListener(new View.OnClickListener() {
 
-            p3DownBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        int score = Integer.parseInt(p2CurrScore.getText().toString());
+                        p2CurrScore.setText(String.valueOf(score + 1));
+                    }
+                });
 
-                @Override
-                public void onClick(View v) {
-                    int score = Integer.parseInt(p3CurrScore.getText().toString());
-                    p3CurrScore.setText(String.valueOf(score - 1));
-                }
-            });
+                p2DownBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        int score = Integer.parseInt(p2CurrScore.getText().toString());
+                        p2CurrScore.setText(String.valueOf(score - 1));
+                    }
+                });
 
-            // player 4
-            p4UpBtn.setOnClickListener(new View.OnClickListener() {
+                // get reset button
+                ImageButton optBtn = findViewById(R.id.playerOptBtn);
 
-                @Override
-                public void onClick(View v) {
-                    int score = Integer.parseInt(p4CurrScore.getText().toString());
-                    p4CurrScore.setText(String.valueOf(score + 1));
-                }
-            });
-
-            p4DownBtn.setOnClickListener(new View.OnClickListener() {
-
-                @Override
-                public void onClick(View v) {
-                    int score = Integer.parseInt(p4CurrScore.getText().toString());
-                    p4CurrScore.setText(String.valueOf(score - 1));
-                }
-            });
+                // set up click listeners for the options button:
+                optBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(GameActivity.this, "Options Button clicked", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                break;
         }
     }
 }
