64f3a1ba51c1108f731419c3c24c80a4f3d18b99
Chris-Guffin <35351149+Chris-Guffin@users.noreply.github.com>
2019-08-07 14:31:06 -0700

    home button added
    home button added for options activity

--- app/src/main/java/com/team2/mtglifetracker/options.java
+++ app/src/main/java/com/team2/mtglifetracker/options.java
@@ -1,10 +1,14 @@
 package com.team2.mtglifetracker;
 
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class options extends AppCompatActivity {
     private Button button;
@@ -12,6 +16,16 @@ public class options extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // this removes the title bar from the activity
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // hide's the title bar
+        getSupportActionBar().hide();
+
+        // enable full screen
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_options);
 
         button = findViewById(R.id.button);
@@ -22,6 +36,21 @@ public class options extends AppCompatActivity {
             }
         });
 
+        //make the home button
+        ImageButton homeBtn = findViewById(R.id.homeBtn);
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPlayerSelect();
+            }
+
+
+        });
         }
+
+    private void openPlayerSelect() {
+        Intent intent = new Intent(this, PlayerSelectActivity.class);
+        startActivity(intent);
     }
+}
 
